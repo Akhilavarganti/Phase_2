b3998db9b318353c4dbdb76c5f47a9939456ed96
Akhilavarganti <vargantiakhila@gmail.com>
2026-07-08 13:45:23 +0530

    report

--- Interface_bringup.py
+++ Interface_bringup.py
@@ -6,7 +6,7 @@ import sys
 class PiHardwareInit:
     def __init__(self):
         self.reboot_required = False
-        self.config_file = "/boot/config.txt"
+        self.config_file = "/boot/firmware/config.txt"
 
     # ---------------------------
     # Command runner
